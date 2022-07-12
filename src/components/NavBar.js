5a65f20402184810a7eb5bdfcb45c94491de5d4b
William Rojas <worm4worm@gmail.com>
2022-07-11 20:20:38 -0500

    creación NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../images/logo.jpg';
+import '../styles/NavBar.css';
+
+
+const NavBar = () => {
+  return (
+    <div>
+        <header>
+        <a href='/#' className='logo'>
+            <img src={ logo } alt="Logo de la empressa" className="Imagen Logo"/>
+            <h2>Worm Sports</h2>
+        </a>
+        
+        <nav>
+            <ul className="nav justify-content-end">
+                <li>
+                  <a href='/#'>Inicio</a>
+                </li>
+                <li>
+                  <a href='/#'>Adidas</a>
+                </li>
+                <li>
+                  <a href='/#'>Nike</a>
+                </li>
+              </ul>
+        </nav>
+        <div className='carrito'>
+        <box-icon name='cart-download' ></box-icon>
+            <span className='item'>0</span>
+        </div>
+      </header> 
+    </div>
+  )
+}
+
+export default NavBar
