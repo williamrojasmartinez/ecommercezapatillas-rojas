e7dc2f63734c5d7fd9d5fe9fb58aaee48f325618
William Rojas <worm4worm@gmail.com>
2022-09-02 21:37:46 -0500

    Ocultando boton de agregar al carrito

--- src/components/detail/ItemDetail.js
+++ src/components/detail/ItemDetail.js
@@ -14,11 +14,13 @@ function ItemDetail({  titulo, image, descripcion, stock, precio, id }) {
   const onAdd = (cantidad, tallas) => {
     const tenis = {id, titulo, image, descripcion, stock, precio, tallas}
 
-    document.getElementById("agregar").style.display = "none";
+    
+    
 
     const elementoActivo = document.querySelector('input[name="talla"]:checked')
 
     if(elementoActivo){
+      document.getElementById("agregar").style.display = "none";
       console.log(`Agregaste ${cantidad} al carrito`)
       console.log(`Talla: ${elementoActivo.value}`)
 
